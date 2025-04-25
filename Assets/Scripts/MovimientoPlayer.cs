f2d65a06ab94dc5f35e301e6af857871b4cf89d8
JoaoConde12 <132232545+JoaoConde12@users.noreply.github.com>
2025-04-25 05:07:00 -0500

    Agregadas animaciones de salto, correr y idle al personaje

--- Assets/Scripts/MovimientoPlayer.cs
+++ Assets/Scripts/MovimientoPlayer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MovimientoPlayer : MonoBehaviour
 {
     public float velocidad = 4f;
+    public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +16,17 @@ public class MovimientoPlayer : MonoBehaviour
     void Update()
     {
         float velocidadX = Input.GetAxis("Horizontal") * Time.deltaTime * velocidad;
+        animator.SetFloat("Movement", velocidadX * velocidad);
+
+        if (velocidadX < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        if (velocidadX > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
         Vector3 posicion = transform.position;
         transform.position = new Vector3(velocidadX + posicion.x, posicion.y, posicion.z);
     }
